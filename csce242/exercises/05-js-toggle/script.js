b33c6072dd05b1f5db862a14d3ce066866cdc941
alexamico1255 <alexamico1255@gmail.com>
2024-05-15 11:55:37 -0400

    exercise 05

---
+++ csce242/exercises/05-js-toggle/script.js
@@ -0,0 +1,59 @@
+//toggling the navigation bar
+document.getElementById("toggle").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+}
+
+const moveBall = () => {
+    const root = document.querySelector(":root");
+    root.style.setProperty("--ball-left", ballLeft + "px");
+    root.style.setProperty("--ball-top", ballTop + "px");
+}
+
+//moving to the right
+let ballLeft = 0;
+let ballTop = 0;
+document.getElementById("right-arrow").onclick = () => {
+    ballLeft += 10;
+    moveBall();
+}
+
+document.getElementById("left-arrow").onclick = () => {
+    ballLeft -= 10;
+    moveBall();
+}
+
+document.getElementById("top-arrow").onclick = () => {
+    ballTop -= 10;
+    moveBall();
+}
+
+document.getElementById("bottom-arrow").onclick = () => {
+    ballTop += 10;
+    moveBall();
+}
+
+//Color Mood Message Generator
+document.getElementById("txt-color").onkeyup = e => {
+    //const color = document.getElementById("txt-color").value;
+    //console.log(color);
+
+    const color = e.currentTarget.value.toLowerCase();
+    const messageP = document.getElementById("color-message");
+
+    
+    if(color =="red") {
+        messageP.innerHTML = "You are ANGRY!"
+        messageP.style.color = "red";
+    }
+    else if(color == "blue") {
+        messageP.innerHTML = "You are MOODY!"
+        messageP.style.color = "blue";
+
+
+    }
+    else {
+        messageP.innerHTML = "I don't know what you are! Try following directions!"
+
+    }
+
+}
